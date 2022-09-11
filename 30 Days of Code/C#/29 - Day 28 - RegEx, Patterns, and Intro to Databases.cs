7e7c9e43604f4afd194fe2bc95edbc87ff70fa69
Md Layek <76832942+md-layek@users.noreply.github.com>
2022-09-11 16:36:15 +0600

    Add 30 Days of Code C# Solutions
    I updated RegEx, Patterns and Intro to Databases challenge solution ...

--- 30 Days of Code/C#/29 - Day 28 - RegEx, Patterns, and Intro to Databases.cs
+++ 30 Days of Code/C#/29 - Day 28 - RegEx, Patterns, and Intro to Databases.cs
@@ -11,30 +11,55 @@
 //         Solution
 // ========================
 
-using System;
+
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-class Solution {
-    static void Main(String[] args) {
-        var n = int.Parse(Console.ReadLine());
 
-        var list = new List < string > ();
 
-        for (int i = 0; i < n; i++) {
-            var tmp = Console.ReadLine().Split(' ');
-            var name = tmp[0];
-            var email = tmp[1];
+class Solution
+{
+    public static void Main(string[] args)
+    
+    {
+        int N = Convert.ToInt32(Console.ReadLine().Trim());
+        
+        var list =new List<string>();
 
-            if (Regex.IsMatch(email, @".+@gmail\.com$")) {
-                list.Add(name);
+        for (int NItr = 0; NItr < N; NItr++)
+        {
+            string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+            string firstName = firstMultipleInput[0];
+
+            string emailID = firstMultipleInput[1];
+            
+            if(Regex.IsMatch(emailID, @".+@gmail\.com$")){
+                
+                list.Add(firstName);
+             
             }
-        }
-
-        list.Sort();
-
-        foreach(var name in list) {
-            Console.WriteLine(name);
-        }
+       }
+        
+       list.Sort();
+    
+       foreach(var firstName in list){
+           
+           Console.WriteLine(firstName);
+           
+           }
+           
     }
+    
 }
